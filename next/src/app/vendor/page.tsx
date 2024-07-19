d2c46b2fb7445275540c5644461b0f01135a5d8c
henriquemantovan <henrique.mantovan@usp.br>
2024-07-19 20:04:43 -0300

    arrumando a interface do vendor

--- next/src/app/vendor/page.tsx
+++ next/src/app/vendor/page.tsx
@@ -3,21 +3,20 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-xl mb-8">Vendor Page</div>
-      <div className="flex flex-col items-center space-y-4">
-        <header>
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '0px' }}>
+      <div style={{ fontSize: '2rem', fontWeight: '700', textAlign: 'center' }}>Vendor Page</div>
+      <br></br>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '16px' }}>
+        <div style={{ display: 'flex', gap: '16px' }}>
           <Link href="vendor/registrated">
             <button className="px-4 py-2 border rounded">Already Vendor</button>
           </Link>
-        </header>
-        <header>
           <Link href="vendor/registration">
             <button className="px-4 py-2 border rounded">
               Become a Vendor
             </button>
           </Link>
-        </header>
+        </div>
       </div>
     </main>
   );
